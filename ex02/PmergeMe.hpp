018b35b36850ad6b9d1c905782b466cb21699bee
Erya Wu <ewu@2-G-2.42heilbronn.de>
2025-08-16 11:21:04 +0200

    notes and comments

--- ex02/PmergeMe.hpp
+++ ex02/PmergeMe.hpp
@@ -6,7 +6,7 @@
 /*   By: ewu <ewu@student.42heilbronn.de>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/05 15:48:02 by ewu               #+#    #+#             */
-/*   Updated: 2025/08/15 20:46:12 by ewu              ###   ########.fr       */
+/*   Updated: 2025/08/16 11:20:52 by ewu              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,30 +14,26 @@
 #define PMERGEME_HPP
 
 #include <vector>
+#include <list>
+#include <iostream>
+#include <ctime>
+#include <climits>
 #include <deque>
 #include <algorithm>
 #include <stdexcept>
 #include <cstdlib>
 
+
+/**
+ * FUNCTION:
+ * createPair(); sortPaor();
+ * bigChain();
+ * smallCh(); => insert to bigChain() using Jacobthals number, binary search
+ * QUESTION: why jacob number??
+ * 
+ */
 class PmergeMe
 {
-private:
-	std::vector<int> _vector;
-	std::deque<int> _deque;
-
-	// create jacobsthal number order for insertion
-	static std::vector<size_t> _calJacob(size_t n);
-	
-	// main algorithm, create main_chain and pending_chain, 
-	// insert P to M with Jacob order with binary search
-	void _vectorSort(int _begin, int _end);
-	void _dequeSort(int _begin, int _end);
-
-	// utilities
-	void _insertVec(int _begin, int _end);
-	void _insertDeque(int _begin, int _end);
-	bool _vecDup();
-	bool _dequeDup();
 	
 public:
 	PmergeMe();
